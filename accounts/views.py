d429e35ff4ef1c13eafa50abb02ae30da37d081f
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-10 16:04:54 +0000

    added user_login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
