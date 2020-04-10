853de95af53b4e51c1f9af25561621a465bf747a
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-10 16:08:22 +0000

    added accounts urls to project urls

--- truebrew/urls.py
+++ truebrew/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pages.views import home_page
+from accounts import urls as accounts_urls
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,4 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name="home"),
+    path('accounts/', include(accounts_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
