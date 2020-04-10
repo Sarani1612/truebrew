d93a02c7cc6130b14de5550ac66e1ff359f1c88d
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-10 16:07:55 +0000

    added accounts app to settings.py

--- truebrew/settings.py
+++ truebrew/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_forms_bootstrap',
+    'accounts',
     'pages',
     'products',
 ]
