5224508b95f5be8cc4af2488b6a45276c6b5a727
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-11 09:56:36 +0000

    added url for registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import user_login, user_logout, user_account
+from .views import user_login, user_logout, user_account, user_registration
 
 
 urlpatterns = [
     path('login/', user_login, name="login"),
     path('logout/', user_logout, name="logout"),
     path('account/', user_account, name="account"),
+    path('register/', user_registration, name="registration"),
 ]
