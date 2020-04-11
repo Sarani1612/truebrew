1f1bf207700fd915ddc22f30b1aacaed533a225c
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-11 09:31:17 +0000

    added url for user_account function

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import user_login, user_logout
+from .views import user_login, user_logout, user_account
 
 
 urlpatterns = [
     path('login/', user_login, name="login"),
     path('logout/', user_logout, name="logout"),
+    path('account/', user_account, name="account"),
 ]
