5733378bab96f64e912cd870d49296a68d75e6b8
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-11 08:27:34 +0000

    added messages to login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 # Create your views here.
@@ -10,8 +11,10 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, "You have successfully logged in!")
             return redirect('home')
         else:
+            messages.success(request, 'Login failed - please try again')
             return redirect('login')
     else:
         return render(request, 'login.html', {})
