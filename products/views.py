fe55f81c9bfdd9b9c9e2e269f1044474af7471ff
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-13 13:07:48 +0000

    added subscriptions to product view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Subscription
 
 # gets products to populate navbar dropdown in all views
 products = Product.objects.all()
@@ -7,4 +7,10 @@ products = Product.objects.all()
 
 def view_product(request, pk):
     tea = get_object_or_404(Product, pk=pk)
-    return render(request, "product.html", {'product': tea, 'products': products})
+    subs = Subscription.objects.all()
+    context = {
+        'product': tea,
+        'subscriptions': subs,
+        'products': products
+    }
+    return render(request, "product.html", context)
