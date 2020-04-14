3c727cd3c145591c77c4945994f58361fbba57fe
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-14 09:18:53 +0000

    created UserInfo model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    street_address1 = models.CharField(max_length=40, blank=True)
+    street_address2 = models.CharField(max_length=40, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=True)
+    postcode = models.CharField(max_length=20, blank=True)
+    county = models.CharField(max_length=40, blank=True)
+    country = models.CharField(max_length=40, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.user.username
