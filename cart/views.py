914cca15c5ecd1af345ffd9bfe9b280677bb1084
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-15 14:19:14 +0000

    added view_cart view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from products.models import Product
+
+
+# gets products to populate navbar dropdown in all views
+products = Product.objects.all()
+
 
 # Create your views here.
+def view_cart(request):
+    return render(request, 'cart.html', {'products': products})
