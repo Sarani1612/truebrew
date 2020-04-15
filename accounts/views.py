ba1969181ea7c1db0acabc5538343ae5f8a67b35
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-15 09:33:57 +0000

    created edit_account function

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, EditUserInfoForm, EditUserForm
 from products.models import Product
 
 # gets products to populate navbar dropdown in all views
 products = Product.objects.all()
+
 
 # Create your views here.
 def user_login(request):
@@ -53,3 +54,26 @@ def user_registration(request):
 def user_account(request):
     '''The users profile page'''
     return render(request, 'account.html', {'products': products})
+
+
+@login_required
+def edit_account(request):
+    if request.method == 'POST':
+        user_form = EditUserForm(request.POST, instance=request.user)
+        user_info_form = EditUserInfoForm(request.POST, instance=request.user.userinfo)
+        if user_form.is_valid() and user_info_form.is_valid():
+            user_form.save()
+            user_info_form.save()
+            messages.success(request, "Your profile has been updated!")
+            return redirect('account')
+        else:
+            messages.error(request, 'Your form has errors', extra_tags='danger')
+    else:
+        user_form = EditUserForm(instance=request.user)
+        user_info_form = EditUserInfoForm(instance=request.user.userinfo)
+    context = {
+        'products': products,
+        'user_form': user_form,
+        'user_info_form': user_info_form
+        }
+    return render(request, 'editaccount.html', context)
