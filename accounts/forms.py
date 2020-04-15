4754388fbba4e95051a53868bfc78ab0fe5aa89f
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-15 09:33:11 +0000

    added UserChangeForms for the user to be able to edit their info

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from .models import UserInfo
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -9,3 +10,15 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class EditUserForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+
+class EditUserInfoForm(UserChangeForm):
+    class Meta:
+        model = UserInfo
+        fields = ['street_address1', 'street_address2', 'town_or_city', 'postcode', 'county', 'country', 'phone_number']
