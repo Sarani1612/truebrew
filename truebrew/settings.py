f1052ce59d0cb291570fec61834f07cbc81d442b
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-15 06:30:18 +0000

    changed database settings

--- truebrew/settings.py
+++ truebrew/settings.py
@@ -81,15 +81,15 @@ WSGI_APPLICATION = 'truebrew.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
