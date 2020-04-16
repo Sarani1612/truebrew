cc4b29b20af03f3ab87c1d82e51620312467e894
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-16 06:35:56 +0000

    changed method for getting subscriptions in view_product view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Subscription
+from .models import Product
 
 # gets products to populate navbar dropdown in all views
 products = Product.objects.all()
@@ -7,7 +7,8 @@ products = Product.objects.all()
 
 def view_product(request, pk):
     tea = get_object_or_404(Product, pk=pk)
-    subs = Subscription.objects.all()
+    subs = tea.subscriptions.all()
+
     context = {
         'product': tea,
         'subscriptions': subs,
