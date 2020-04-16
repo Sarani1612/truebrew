8c54ae3ce468076dfbe945a339195a9e300bee25
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-16 06:19:50 +0000

    added ManyToManyField to Product to link it to subscriptions

--- products/models.py
+++ products/models.py
@@ -1,18 +1,30 @@
 from django.db import models
 
 
+class Subscription(models.Model):
+    frequency = models.CharField(max_length=15)
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    practical_info = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.frequency
+
+
 class Product(models.Model):
     BLACK_TEA = 'Black Tea'
     GREEN_TEA = 'Green Tea'
     WHITE_TEA = 'White Tea'
     ROOIBOS = 'Rooibos'
     MIX = 'Mix'
+    OOLONG = 'Oolong'
     TEA_CHOICES = [
         (BLACK_TEA, 'Black Tea'),
         (GREEN_TEA, 'Green Tea'),
         (WHITE_TEA, 'White Tea'),
         (ROOIBOS, 'Rooibos'),
         (MIX, 'Mix'),
+        (OOLONG, 'Oolong')
     ]
     title = models.CharField(max_length=50)
     category = models.CharField(
@@ -23,16 +35,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='images')
     description = models.TextField()
     short_description = models.TextField()
+    subscriptions = models.ManyToManyField(Subscription)
 
     def __str__(self):
         return self.title
-
-
-class Subscription(models.Model):
-    frequency = models.CharField(max_length=15)
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    practical_info = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.frequency
