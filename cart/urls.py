c700a99799a0937ff300262c3a3b2b9a37abd98c
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-16 08:41:46 +0000

    added add_to_cart url

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import view_cart
+from .views import view_cart, add_to_cart
 
 
 urlpatterns = [
     path('', view_cart, name="cart"),
+    path('add/<int:id>/', add_to_cart, name="add_to_cart"),
 ]
