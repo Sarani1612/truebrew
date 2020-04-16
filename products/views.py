a06badfdc8dc9c3a56e35cc4a08e311931fc4ffc
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-16 15:42:28 +0000

    added subscription filter to view_product view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Subscription
 
 # gets products to populate navbar dropdown in all views
 products = Product.objects.all()
@@ -7,7 +7,7 @@ products = Product.objects.all()
 
 def view_product(request, pk):
     tea = get_object_or_404(Product, pk=pk)
-    subs = tea.subscriptions.all()
+    subs = Subscription.objects.filter(product__exact=tea)
 
     context = {
         'product': tea,
