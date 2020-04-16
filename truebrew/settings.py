b6a6ba5eadbf84130cd733fd772f2addfe4033a0
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-16 08:39:26 +0000

    added cart contexts to settings

--- truebrew/settings.py
+++ truebrew/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 if os.path.exists("env.py"):
-  import env
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
         },
     },
