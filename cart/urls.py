b7be9208e59f60b6a8edd2e0af6d8b3c954b870a
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-17 10:34:27 +0000

    added path for adjusting number of items in cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import view_cart, add_to_cart
+from .views import view_cart, add_to_cart, adjust_quantity
 
 
 urlpatterns = [
     path('', view_cart, name="cart"),
     path('add/<int:id>/', add_to_cart, name="add_to_cart"),
+    path('adjust/<int:id>/', adjust_quantity, name="adjust_quantity"),
 ]
