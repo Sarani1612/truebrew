64d5db282362f63c798e8f95a41232ba90b83cbc
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-17 11:49:05 +0000

    added view and path for viewing all products

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import view_product
+from .views import view_product, all_products
 
 
 urlpatterns = [
+    path('', all_products, name="all_products"),
     path('<int:pk>/', view_product, name="view_product"),
 ]
--- products/views.py
+++ products/views.py
@@ -5,7 +5,16 @@ from .models import Product, Subscription
 products = Product.objects.all()
 
 
+def all_products(request):
+    ''' renders the allproducts template showing a card for each tea '''
+    return render(request, 'allproducts.html', {'products': products})
+
+
 def view_product(request, pk):
+    '''
+    product view showing a card for a given tea
+    and the available subscriptions
+    '''
     tea = get_object_or_404(Product, pk=pk)
     subs = Subscription.objects.filter(
         product__exact=tea).order_by('unit_price')
