95362a04c84ec51cea4c064707e943031932c813
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-19 08:01:31 +0000

    added custom labels for forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,11 +7,17 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 class UserRegistrationForm(UserCreationForm):
     '''Form to create a user account'''
-    email = forms.EmailField()
+    email = forms.EmailField(label='Enter your email address:')
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Enter a username:'
+        self.fields['password1'].label = 'Enter a password:'
+        self.fields['password2'].label = 'Enter the password again:'
 
 
 class EditUserForm(UserChangeForm):
@@ -26,6 +32,13 @@ class EditUserForm(UserChangeForm):
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'username:'
+        self.fields['email'].label = 'Email Address:'
+        self.fields['first_name'].label = 'First Name:'
+        self.fields['last_name'].label = 'Last Name:'
+
 
 class EditUserInfoForm(ModelForm):
     ''' Form to be used by a user in order to edit their account details '''
@@ -33,3 +46,13 @@ class EditUserInfoForm(ModelForm):
         model = UserInfo
         fields = ['street_address1', 'street_address2', 'town_or_city',
                   'postcode', 'county', 'country', 'phone_number']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['street_address1'].label = 'Street Address:'
+        self.fields['street_address2'].label = 'Street Address 2:'
+        self.fields['town_or_city'].label = 'Town/City:'
+        self.fields['postcode'].label = 'Postcode:'
+        self.fields['county'].label = 'County/State:'
+        self.fields['country'].label = 'Country:'
+        self.fields['phone_number'].label = 'Phone Number:'
