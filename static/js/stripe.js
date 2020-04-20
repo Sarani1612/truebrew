41da7c9a372753b2d648cf290451d6e9a556b33a
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-20 13:58:41 +0000

    added stripe js file

---
+++ static/js/stripe.js
@@ -0,0 +1,24 @@
+
+
+let stripe = Stripe(publishable);
+let elements = stripe.elements();
+
+
+let style = {
+  base: {
+    color: "#32325d",
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: "antialiased",
+    fontSize: "16px",
+    "::placeholder": {
+      color: "#aab7c4"
+    }
+  },
+  invalid: {
+    color: "#fa755a",
+    iconColor: "#fa755a"
+  }
+};
+
+let cardElement = elements.create("card", { style: style });
+cardElement.mount("#card-element");
