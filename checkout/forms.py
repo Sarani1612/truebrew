20d0d8c5867260d3dfa5136ef8e7f85ecbd24ff6
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-20 16:18:23 +0000

    deleted PaymentForm as it is not needed

--- checkout/forms.py
+++ checkout/forms.py
@@ -2,23 +2,6 @@ from django import forms
 from .models import Order
 
 
-class PaymentForm(forms.Form):
-    '''
-    Form for inputting payment card details
-    '''
-    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
-    YEAR_CHOICES = [(i, i) for i in range(2020, 2040)]
-
-    payment_card_number = forms.CharField(
-        label="Payment card number", required=False)
-    cvv = forms.CharField(label="Security code (CVV)", required=False)
-    expiry_month = forms.ChoiceField(
-        label="Month", choices=MONTH_CHOICES, required=False)
-    expiry_year = forms.ChoiceField(
-        label="Year", choices=YEAR_CHOICES, required=False)
-    stripe_id = forms.CharField(widget=forms.HiddenInput())
-
-
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
