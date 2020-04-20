f6cc835e625061591b9b8d77773ac7489855ccb8
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-20 07:08:32 +0000

    created basic view for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import OrderLineItem
+from .forms import PaymentForm, OrderForm
+from django.conf import settings
+from django.utils import timezone
+import stripe
+from products.models import Product, Subscription
+
+# gets products to populate navbar dropdown in all views
+products = Product.objects.all()
 
 # Create your views here.
+@login_required()
+def checkout(request):
+    context = {
+        'products': products,
+        # 'order_form': order_form,
+        # 'payment_form': payment_form,
+        # 'publishable': settings.STRIPE_PUBLISHABLE
+        }
+    return render(request, "checkout.html", context)
