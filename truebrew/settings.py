1894498b6ed37918ea4c7ad4b9b7170e5d057331
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-22 13:25:58 +0000

    tried changing from TLS to SSL as email not working

--- truebrew/settings.py
+++ truebrew/settings.py
@@ -152,10 +152,10 @@ EMAILJS_USER = os.environ.get('EMAILJS_USER')
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'truebrewboxes@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 
 
 # EMAIL_HOST = 'localhost'
