514899f4b0526b12131780a060226f551175e29c
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-22 05:49:06 +0000

    removed send_message view, moved its functionality into contact_page view

--- pages/views.py
+++ pages/views.py
@@ -18,35 +18,31 @@ def contact_page(request):
     renders the contact page, and if user is logged in,
     will pre-populate 'user' (hidden field) and 'email'.
     '''
-    if request.user.is_authenticated:
-        initial = {
-            'user': request.user,
-            'email': request.user.email
+    if request.method == 'POST':
+        contact_form = ContactMessageForm(request.POST)
+
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(request, 'Your message has been sent!')
+            return redirect('contact')
+        else:
+            messages.error(
+                request,
+                'Your message was not sent. Please try again.', extra_tags='danger')
+            return redirect('contact')
+    else:
+        if request.user.is_authenticated:
+            initial = {
+                'user': request.user,
+                'email': request.user.email
+            }
+            print(initial)
+            contact_form = ContactMessageForm(initial=initial)
+        else:
+            contact_form = ContactMessageForm()
+        context = {
+            'products': products,
+            'contact_form': contact_form,
+            'emailjs_user': settings.EMAILJS_USER
         }
-        print(initial)
-        contact_form = ContactMessageForm(initial=initial)
-    else:
-        contact_form = ContactMessageForm()
-    context = {
-        'products': products,
-        'contact_form': contact_form,
-        'emailjs_user': settings.EMAILJS_USER
-    }
-    return render(request, 'contact.html', context)
-
-
-def send_message(request):
-    '''
-    saves the message to the ContactMessage database
-    '''
-    contact_form = ContactMessageForm(request.POST)
-
-    if contact_form.is_valid():
-        contact_form.save()
-        messages.success(request, 'Your message has been sent!')
-        return redirect('contact')
-    else:
-        messages.error(
-            request,
-            'Your message was not sent. Please try again.', extra_tags='danger')
-        return redirect('contact')
+        return render(request, 'contact.html', context)
