c5dfdee148555c19bb3e5d5b9cdec055a323fa37
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-23 06:17:30 +0000

    moved EMAIL_HOST_USER into env variables

--- truebrew/settings.py
+++ truebrew/settings.py
@@ -153,7 +153,7 @@ EMAILJS_USER = os.environ.get('EMAILJS_USER')
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'truebrewboxes@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True
 
