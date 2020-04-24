b70f21899f527c9295e057d311e40b54ba970721
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-24 09:55:13 +0000

    added password reset urls to urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from .views import user_login, user_logout, user_account, user_registration,        edit_account
+from accounts import url_reset
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('account/', user_account, name="account"),
     path('register/', user_registration, name="registration"),
     path('edit/', edit_account, name="edit_account"),
+    path('reset-password/', include(url_reset))
 ]
