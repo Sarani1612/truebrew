fe85d5b1e0715f88c38fd59f7b3f5b87581f4468
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-24 09:54:41 +0000

    added templates and urls for resetting passwords

---
+++ accounts/url_reset.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
+
+urlpatterns = [
+    path('', PasswordResetView.as_view(), name="reset_password"),
+    path('done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('complete/', PasswordResetCompleteView.as_view(), name="password_reset_complete")
+]
