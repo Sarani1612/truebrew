a18df8f14961f1adc8714e536cbae25bbec7dbd0
Sarani1612 <sara.larsen1612@gmail.com>
2020-04-27 09:12:19 +0000

    added forms testing to accounts app

---
+++ accounts/test_forms.py
@@ -0,0 +1,61 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .forms import UserRegistrationForm, EditUserInfoForm, EditUserForm
+
+# Create your tests here.
+class TestUserRegistrationForm(TestCase):
+
+    def test_register_user(self):
+        form = UserRegistrationForm(
+            {'username': 'User123',
+            'email': 'user@user.com',
+            'password1': 'Teapot486',
+            'password2': 'Teapot486'
+            })
+        self.assertTrue(form.is_valid())
+
+    def test_correct_msg_for_passwords_not_matching(self):
+        form = UserRegistrationForm(
+            {'username': 'User123',
+            'email': 'user@user.com',
+            'password1': 'Teapot486',
+            'password2': 'Teapot485'
+            })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['password2'], [u"The two password fields didn't match."])
+
+    def test_correct_msg_for_invalid_email(self):
+        form = UserRegistrationForm(
+            {'username': 'User123',
+            'email': 'user@user',
+            'password1': 'Teapot486',
+            'password2': 'Teapot486'
+            })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['email'], [u"Enter a valid email address."])
+
+
+class TestEditUserForm(TestCase):
+    def setUp(self):
+        self.testuser = User.objects.create_user(username='johnsmith', email='john@smith.com', password='Teapot486')
+    
+    def tearDown(self):
+        self.testuser.delete()
+
+    def test_edit_user_form(self):
+        user = User.objects.get(id=1)
+        form = EditUserForm()
+        self.assertTrue(form.is_valid())
+
+    def test_edit_user_info_form(self):
+        user = User.objects.get(id=1)
+        form = EditUserInfoForm(
+            {'street_address1': '12 Test St',
+            'street_address2': '',
+            'town_or_city': 'Test City',
+            'postcode': '',
+            'county': 'Cork',
+            'country': 'Ireland',
+            'phone_number': '0123456789'
+            })
+        self.assertTrue(form.is_valid())
